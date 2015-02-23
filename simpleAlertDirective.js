ec53fc0828a84c9883ee2faa9873dc0edae1a85c
John Wundes <johnwun@gmail.com>
2015-02-23 14:56:54 -0800

    Update simpleAlertDirective.js
    update to add timeout

--- simpleAlertDirective.js
+++ simpleAlertDirective.js
@@ -1,29 +1,37 @@
 angular.module('simpleAlert')
-.directive('simpleAlert', function(simpleAlertService) {
+.directive('simpleAlert', function($timeout, simpleAlertFactory) {
   return {
     restrict: 'E',
      scope: { 
        id:'@' 
        //TODO: add timeout value if auto-timeout
     },
-    template: '<div data-ng-click="clickToClose()" class="alert large alert-{{msg.type}} alert-dismissable" ng-show="msg.message.length">'+
+    template: '<div data-ng-click="clickToClose()" class="alert alert-{{msg.type}} alert-dismissable {{msg.classes}}" ng-show="msg.message.length">'+
   '<button class="close" data-dismiss="alert" data-ng-click="clear()" ng-show="msg.closeIcon">'+
-  '<span class="nuxicon nuxicon-close"></span>'+
+  '<span class="{{msg.closeIconClasses}}"></span>'+
   '</button>'+
   '<h3 ng-show="msg.title.length" ng-bind="msg.title"></h3>'+
   '<p>{{msg.message}}</p>'+
   '<button ng-show="msg.cancelLabel.length" class="btn btn-default" data-ng-click="cancel()">{{msg.cancelLabel}}</button>'+
   '<button ng-show="msg.okLabel.length" class="btn btn-{{msg.type}}" data-ng-click="clear()">{{msg.okLabel}}</button>'+
   '</div>',
-    link:function($scope){ 
-      $scope.msgObj = simpleAlertService.messages;
+    link:function($scope){  
+      var timeoutVar;
+      $scope.msgObj = simpleAlertFactory.messages;
       
-      // deep watch - can we avoid?
-      $scope.$watch('msgObj',function(newVal,oldVal){
-         
+      // deep watch - can we avoid?  contains all named alert instances currently in play.
+      $scope.$watch('msgObj',function(newVal,oldVal){  
+        $timeout.cancel( timeoutVar );
          // The id here is the key so newVal[id] is an alert config object
         if(newVal[$scope.id] && ! angular.equals(newVal[$scope.id],oldVal[$scope.id])){ 
-        $scope.msg = newVal[$scope.id];
+          $scope.msg = newVal[$scope.id];
+          
+          // add a timeout if 'timeout exists on msgObj'
+          if(newVal[$scope.id].timeout){ 
+            timeoutVar = $timeout(function(){ 
+              $scope.clear();
+            },+newVal[$scope.id].timeout);
+          }
         }
       },true);
 
@@ -32,17 +40,20 @@ angular.module('simpleAlert')
       };
 
       $scope.clear = function(){ 
+        $timeout.cancel( timeoutVar );
         //removes the  message  and trigger the success handler
-        simpleAlertService.clearById($scope.id);  
+        simpleAlertFactory.clearById($scope.id); 
+        
       };
       
       $scope.cancel = function(){ 
         //removes the  message and trigger the cancel handler
-        simpleAlertService.cancelById($scope.id); 
+        simpleAlertFactory.cancelById($scope.id); 
       };
       
       $scope.$on('$destroy', function() {
-           simpleAlertService.removeById($scope.id); 
+        $timeout.cancel( timeoutVar );
+        simpleAlertFactory.removeById($scope.id); 
       });
       
     }
