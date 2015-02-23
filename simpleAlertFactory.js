f13332585af8d96ee5e05d646989356add87623e
John Wundes <johnwun@gmail.com>
2015-02-23 14:55:37 -0800

    Update simpleAlertFactory.js
    updating to add timeout.

--- simpleAlertFactory.js
+++ simpleAlertFactory.js
@@ -1,12 +1,14 @@
 var msg = angular.module('simpleAlert', []);
-msg.service('simpleAlertService', function() {
-  
+msg.factory('simpleAlertFactory', function($timeout) {
+  var timeoutVar;
   var internal = {
     messages:{},
     default:{
       id:undefined,
       type:'warning', 
-      message:'', 
+      message:'',
+      // custom
+      classes:'',
       
       //OK is optional
       okLabel:'',
@@ -18,27 +20,22 @@ msg.service('simpleAlertService', function() {
       cancelCallback:angular.noop,
       cancelCallbackParamsArray:[undefined],
       title:undefined,
-
+      timeout:0,
       closeIcon:true,
+      closeIconClasses:'glyphicon glyphicon-remove-sign',
       clickToClose:false
     }
   }; 
    
   var show = function(opts){ 
     var msg = angular.extend({},internal.default, opts);
-    internal.messages[msg.id] = msg; 
+    internal.messages[msg.id] = msg;  
   };
   
   var clearById = function(id){ 
     internal.messages[id].callback.apply(this,internal.messages[id].callbackParamsArray);
-    // NOTE: This successfully blanks the value, but fails to 
-    // reset it to it's original undefined state
-    // if used across a large application it will lead to an
-    // empty objects stored in the service for each alert in the app.
-    //
-    // To prevent this (and to prevent errors from crossing page boundaries, 
-    // we call the remove function inside the directives destroy function.
     internal.messages[id] = {};
+    timeoutVar = $timeout(function(){delete internal.messages[id];},0);
   };
   
   var removeById = function(id){
@@ -46,17 +43,33 @@ msg.service('simpleAlertService', function() {
   };
   
   var clearAll = function(){ 
-    angular.forEach(internal.messages, function(value, key) { 
-      internal.messages[key] = {}; 
-    } );
-  };
+    angular.forEach(internal.messages, function(value,key) {  
+     internal.messages[key] = {};   
+    }); 
+    
+    $timeout(function(){
+      angular.forEach(internal.messages, function(value,key) {  
+      delete internal.messages[key];  
+    }); 
+    
+    },0);
+  
+    
+   };
   
   var cancelById = function(id){ 
     internal.messages[id].cancelCallback.apply(this,internal.messages[id].cancelCallbackParamsArray);
-    internal.messages[id] = {}; 
+    internal.messages[id] = {};
+    // delayed delete so everything has a chance to execute.
+   timeoutVar = $timeout(function(){delete internal.messages[id];},0);
+   
   };
   
+  var setDefault = function(key,newDefault){
+    internal.default[key] = newDefault;
+  };
   return { 
+    setDefault:setDefault,
     show:show,
     cancelById:cancelById,
     clearById:clearById,
